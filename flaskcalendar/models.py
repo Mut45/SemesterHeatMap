ca2c4b9ce0584bc1cb8e9641b2db900a4779e8d7
Mut45 <muvetian@gmail.com>
2018-12-18 13:45:23 +0800

    database update #1

---
+++ flaskcalendar/models.py
@@ -0,0 +1,15 @@
+from app import db
+
+
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(64), index=False, unique=False)
+    start = db.Column(db.TIMESTAMP, index=False, unique=False)
+    end = db.Column(db.TIMESTAMP, index=False, unique=False)
+    weight = db.Column(db.String(64), index=False, unique=False)
+    course_id = db.Column(db.String(120), index=False, unique=False)
+    user_id = db.Column(db.String(120), index=False, unique=False)
+    password_hash = db.Column(db.String(128))
+
+    def __repr__(self):
+        return '<User {}>'.format(self.username)
