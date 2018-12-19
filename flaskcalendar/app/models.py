0d54919d4b86f69e003d190c749ff5d22bb293e7
Mut45 <muvetian@gmail.com>
2018-12-19 12:24:33 +0800

    fixed table

--- flaskcalendar/app/models.py
+++ flaskcalendar/app/models.py
@@ -9,7 +9,7 @@ class Assignment(db.Model):
     weight = db.Column(db.String(64), index=False, unique=False)
     course_id = db.Column(db.String(120), index=False, unique=False)
     user_id = db.Column(db.String(120), index=False, unique=False)
-    password_hash = db.Column(db.String(128))
+    #password_hash = db.Column(db.String(128))
 
     def __repr__(self):
         return '<Assignment {}:{}, start:{}, end:{}>'.format(self.id,self.title,self.start,self.end)
